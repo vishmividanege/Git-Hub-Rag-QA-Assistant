54a478c122f981e20f69ced35c53f25d74396f9f
Vishmi Vidanege <vishmividanage15@gmail.com>
2026-03-24 12:58:14 +0530

    Changes in similarity score

--- backend.py
+++ backend.py
@@ -81,5 +81,3 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="localhost", port=8000)
-
-#uvicorn.run(app, host="0.0.0.0", port=8000)
